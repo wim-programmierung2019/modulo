540fcf7e3a9208d76b08fd9d96e02b21d764dd81
Christoph Rieger <rieger.chris@googlemail.com>
2019-07-18 17:11:12 +0000

    Modulo stub

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    
+    /**
+     * Modulo
+     */
+    public static void main(String[] args){
+        // Berechnen Sie den Modulowert einer Anfangszahl und einer zweiten Zahl 
+        // durch die geteilt wird, d.h. den “Rest” nach dem Teilen. 
+        // Beispiel 17 mod 5 = 2
+
+    }
+    
+}

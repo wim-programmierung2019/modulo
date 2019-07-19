90dcd596f3d7a248412f48fe9398e0baed2b72ae
jonassmolarek <jonas.smolarek@agravis.de>
2019-07-19 15:14:10 +0000

    fertig

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,22 @@ public class Main {
         // Berechnen Sie den Modulowert einer Anfangszahl und einer zweiten Zahl 
         // durch die geteilt wird, d.h. den “Rest” nach dem Teilen. 
         // Beispiel 17 mod 5 = 2
-
+        int zahl1; 
+        int zahl2; 
+        int durchlauf = 0;
+        
+        zahl1 = 17;
+        zahl2 = 5;
+        
+        System.out.println("Erste Zahl: " + zahl1 + " Zweite Zahl: " + zahl2);
+        
+        while(zahl1 >= zahl2)
+        {
+            zahl1 = zahl1 - zahl2;
+            durchlauf += 1;
+        }
+         System.out.println("Restwert: " + zahl1);
+         System.out.println("Durchlauf: " + durchlauf);
     }
     
 }
